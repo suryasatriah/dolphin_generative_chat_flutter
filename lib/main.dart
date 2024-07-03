0a178619a70c786016206b5621ce7ba9c434d6a0
suryahidayat <surya.hidayat@inmotion.co.id>
2024-07-03 18:58:36 +0700

    add basic chat feature

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,130 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: const TextTheme(
+        bodySmall: TextStyle(fontSize: 10),
+      )),
+      home: const ChatDetail(),
+    );
+  }
+}
+
+class ChatMessage {
+  String messageContent;
+  String messageType;
+  ChatMessage({required this.messageContent, required this.messageType});
+}
+
+List<ChatMessage> messages = [
+  ChatMessage(messageContent: "Hello, Will Smith", messageType: "receiver"),
+  ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+  ChatMessage(
+      messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+      messageType: "sender"),
+  ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+  ChatMessage(
+      messageContent: "Is there any thing wrong?", messageType: "sender"),
+  ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+  ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+  ChatMessage(
+      messageContent: "Hey Kriss, I am doing fine dude. wbu?",
+      messageType: "sender"),
+  ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+  ChatMessage(
+      messageContent: "Is there any thing wrong?", messageType: "sender")
+];
+
+class ChatDetail extends StatelessWidget {
+  const ChatDetail({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(title: const Text("Chat")),
+      body: Container(
+        color: Color(Colors.black.hashCode),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: messages.length,
+                shrinkWrap: true,
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: const EdgeInsets.only(
+                        left: 16, right: 16, top: 10, bottom: 10),
+                    child: Align(
+                      alignment: (messages[index].messageType == "receiver"
+                          ? Alignment.topLeft
+                          : Alignment.topRight),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: (messages[index].messageType == "receiver"
+                              ? Colors.grey.shade200
+                              : Colors.blue[200]),
+                        ),
+                        padding: const EdgeInsets.all(16),
+                        child: Text(messages[index].messageContent,
+                            style: TextStyle(
+                                color: messages[index].messageType == "receiver"
+                                    ? Colors.black
+                                    : Colors.white),
+                      ),
+                    ),
+                  ));
+                },
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: TextFormField(
+                      minLines: 1,
+                      maxLines: 3,
+                      decoration: const InputDecoration(
+                        labelText: "Write your messages",
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                          borderSide: BorderSide.none, // Remove the border
+                        ),
+                        prefixIcon: IconButton(
+                            onPressed: null,
+                            icon: Icon(
+                              Icons.attach_file,
+                              color: Colors.black,
+                            )),
+                      ),
+                      keyboardType: TextInputType.multiline,
+                      onChanged: (String value) {},
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FloatingActionButton(
+                    onPressed: () {},
+                    backgroundColor: Colors.blue,
+                    child: const Icon(
+                      Icons.send,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
         ),
       ),
     );
